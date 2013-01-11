156811a6f255a960af8aef5d1f4a5c7456611b74
xuanyan <xuanyan1983@gmail.com>
2013-01-11 15:05:04 +0800

    sss

--- Config.php
+++ Config.php
@@ -2,7 +2,8 @@
 
 return array(
     'servers' => array(
-        '173.192.165.130:1234' => array('www.issh.in', '6K97nz')
+        //'173.192.165.130:1234' => array('www.issh.in', '6K97nz'),
+        '209.141.62.117:443' => array('xua785g', 'ukuwonyo')
     ),
     'database' => array(
             'connection' => getenv('CLEARDB_DATABASE_URL'),
